3128f72c2e03a2968115e0c7ba0964ef5a71ac2a
dhiogoacioli <dhiogoacioli@gmail.com>
2020-12-13 20:18:44 -0300

    preparando para apk

---
+++ src/Shared/Seed/EventSeed.cs
@@ -0,0 +1,36 @@
+﻿using Bogus;
+using System.Linq;
+using VerusDate.Shared.Enum;
+using VerusDate.Shared.ViewModel.Command;
+
+namespace VerusDate.Shared.Seed
+{
+    public static class EventSeed
+    {
+        public static Faker<EventVM> GetEventVM(string IdEvent = null, string IdUser = null)
+        {
+            var fakeBD = new Faker<EventVM>("pt_BR")
+                .Rules((s, p) =>
+                {
+                    p.NewBlindDate(
+                        s.Date.FutureOffset(),
+                        s.Address.City(),
+                        s.Random.Number(18, 120),
+                        s.Random.Number(18, 120),
+                        s.Random.ArrayElements(new Intent[] { Intent.OneNightStand, Intent.FriendsWithBenefits, Intent.Relationship, Intent.Married }),
+                        s.Random.ArrayElements(new SexualOrientation[] { SexualOrientation.Assexual, SexualOrientation.Heteressexual, SexualOrientation.Bissexual, SexualOrientation.Bissexual }),
+                        s.Random.Bool()
+                        );
+                }).Generate();
+
+            return new Faker<EventVM>("pt_BR")
+                .Rules((s, p) =>
+                {
+                    p.LoadDefatultData();
+                    p.IdEvent = IdEvent ?? s.Random.Guid().ToString();
+                    p.IdUser = IdUser ?? s.Random.Guid().ToString();
+                    p.NewBlindDate(fakeBD.DtStart, fakeBD.Location, fakeBD.MinimalAge, fakeBD.MaxAge, fakeBD.Intent.ToArray(), fakeBD.SexualOrientation, fakeBD.GenderDivision);
+                });
+        }
+    }
+}
--- src/Shared/Seed/ProfileLookingSeed.cs
+++ src/Shared/Seed/ProfileLookingSeed.cs
@@ -4,45 +4,34 @@ using VerusDate.Shared.ViewModel.Command;
 
 namespace VerusDate.Shared.Seed
 {
-    public static class ProfileSeed
+    public static class ProfileLookingSeed
     {
-        public static Faker<ProfileVM> GetProfileVM(string IdUser = null)
+        public static Faker<ProfileLookingVM> GetProfileLookingVM(string IdUser = null)
         {
-            return new Faker<ProfileVM>("pt_BR")
+            return new Faker<ProfileLookingVM>("pt_BR")
                 .Rules((s, p) =>
                 {
                     p.IdUser = IdUser ?? s.Random.Guid().ToString();
-                    p.NickName = s.Internet.UserName();
-                    p.Description = s.Lorem.Word();
-                    p.BirthDate = s.Date.Past(18).Date;
+                    p.MinimalAge = s.Random.Int(18, 120);
+                    p.MaxAge = s.Random.Int(18, 120);
                     p.BiologicalSex = s.PickRandom<BiologicalSex>();
                     p.MaritalStatus = s.PickRandom<MaritalStatus>();
                     p.Intent = s.Random.ArrayElements(new Intent[] { Intent.OneNightStand, Intent.FriendsWithBenefits, Intent.Relationship, Intent.Married });
                     p.GenderIdentity = s.PickRandom<GenderIdentity>();
                     p.SexualOrientation = s.PickRandom<SexualOrientation>();
-                    p.Location = $"{s.Address.Country()} - {s.Address.State()} - {s.Address.City()}";
                     p.Smoke = s.PickRandom<Smoke>();
                     p.Drink = s.PickRandom<Drink>();
                     p.Diet = s.PickRandom<Diet>();
-                    p.Height = s.PickRandom<Height>();
+                    p.MinimalHeight = s.PickRandom<Height>();
+                    p.MaxHeight = s.PickRandom<Height>();
                     p.BodyMass = s.PickRandom<BodyMass>();
                     p.RaceCategory = s.PickRandom<RaceCategory>();
-                    p.Latitude = s.Address.Latitude(-3.220192, -34.316614);
-                    p.Longitude = s.Address.Longitude(-35.039519, -69.421414);
-                    //p.Latitude = s.Address.Latitude();
-                    //p.Longitude = s.Address.Longitude();
+                    p.Distance = s.Random.Int(0, 10000);
                     p.CareerCluster = s.PickRandom<CareerCluster>();
                     p.EducationLevel = s.PickRandom<EducationLevel>();
-                    p.MoneyPersonality = s.PickRandom<MoneyPersonality>();
-                    p.MyersBriggsTypeIndicator = s.PickRandom<MyersBriggsTypeIndicator>();
                     p.HaveChildren = s.PickRandom<HaveChildren>();
-                    p.RelationshipPersonality = s.PickRandom<RelationshipPersonality>();
                     p.Religion = s.PickRandom<Religion>();
                     p.WantChildren = s.PickRandom<WantChildren>();
-                    p.UpdateMainPhoto(s.Image.PicsumUrl());
-                    p.UpdatePhotoGallery(new[] { s.Image.PicsumUrl() });
-                    p.Hobbies = s.Random.WordsArray(1, 8);
-                    p.ActivityStatus = s.PickRandom<ActivityStatus>();
                 });
         }
     }

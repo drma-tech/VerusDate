950650b3345bf1669370c30b2242f9b7d92d237a
dhiogoacioli <dhiogoacioli@gmail.com>
2020-12-19 18:11:40 -0300

    blazor wasm

---
+++ src/Shared/Entity/Suggestions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using VerusDate.Shared.Core;
+
+namespace VerusDate.Shared.Entity
+{
+    public class Suggestions : EntityType
+    {
+        [Key]
+        public string IdUser { get; set; }
+
+        [Key]
+        public string IdUserSuggestion { get; set; }
+
+        public DateTimeOffset DtExpiration { get; set; }
+    }
+}
---
+++ src/VerusDate.Web/Program.cs
@@ -0,0 +1,30 @@
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace VerusDate.Web
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("#app");
+
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+            builder.Services.AddOidcAuthentication(options =>
+            {
+                // Configure your authentication provider options here.
+                // For more information, see https://aka.ms/blazor-standalone-auth
+                builder.Configuration.Bind("DRMA", options.ProviderOptions);
+                options.ProviderOptions.ResponseType = "code";
+            });
+
+            await builder.Build().RunAsync();
+        }
+    }
+}

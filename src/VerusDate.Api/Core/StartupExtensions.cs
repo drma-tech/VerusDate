f5aaf6ee6971bf2557bc267addab12d354918a17
dhiogoacioli <dhiogoacioli@gmail.com>
2020-12-22 18:41:52 -0300

    ..

--- src/VerusDate.Api/Core/StartupExtensions.cs
+++ src/VerusDate.Api/Core/StartupExtensions.cs
@@ -1,6 +1,7 @@
 ﻿using FluentValidation;
 using MediatR;
 using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
 using VerusDate.Api.Mediator.Behavior;
 using VerusDate.Api.Mediator.Command.Profile;
 using VerusDate.Shared.Validation;
@@ -11,7 +12,7 @@ namespace VerusDate.Api.Core
     {
         public static void AddHandles(this IServiceCollection services)
         {
-            services.AddMediatR(typeof(ProfileAddHandler)); //needs only one class
+            services.AddMediatR(Assembly.GetExecutingAssembly()); //needs only one class
         }
 
         public static void AddPipelines(this IServiceCollection services)

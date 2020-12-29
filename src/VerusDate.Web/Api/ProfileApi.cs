f989104f502a09631b4542c00f5a1115c8bec224
dhiogoacioli <dhiogoacioli@gmail.com>
2020-12-29 19:01:48 -0300

    ..

--- src/VerusDate.Web/Api/ProfileApi.cs
+++ src/VerusDate.Web/Api/ProfileApi.cs
@@ -29,15 +29,6 @@ namespace VerusDate.Web.Api
             return await http.GetCustomSession<Profile>(session, StorageKey + IdUserView, $"Profile/GetView?id={IdUserView}");
         }
 
-        public async static Task<string> Profile_GetView2(this HttpClient http, ISessionStorageService session, string IdUserView)
-        {
-            var response = await http.GetAsync(http.BaseApi() + $"Profile/GetView?id={IdUserView}");
-
-            return await response.Content.ReadAsStringAsync();
-
-            //return await http.GetCustomSession<string>(session, StorageKey + IdUserView, );
-        }
-
         //public List<AffinityVM> GetAffinity(ProfileLooking profUser, Profile profView)
         //{
         //    if (profUser == null) throw new ArgumentNullException(nameof(profUser));

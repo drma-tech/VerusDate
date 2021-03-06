57edf5e755161553d7781d999a15de1e331c86b8
dhiogoacioli <dhiogoacioli@gmail.com>
2021-03-06 12:13:01 -0300

    habilitando o chat por signalr

---
+++ src/VerusDate.Api/Function/SignalRFunction.cs
@@ -0,0 +1,36 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Azure.WebJobs.Extensions.SignalRService;
+using System.Threading.Tasks;
+using VerusDate.Api.Core;
+using VerusDate.Shared.Model;
+
+namespace VerusDate.Api.Function
+{
+    public class SignalRFunction
+    {
+        // This will manage connections to SignalR
+        [FunctionName("negotiate")]
+        public static SignalRConnectionInfo GetSignalRInfo(
+            [HttpTrigger(AuthorizationLevel.Anonymous, FunctionMethod.POST)] HttpRequest req,
+            [SignalRConnectionInfo(HubName = "chatHub")] SignalRConnectionInfo connectionInfo)
+        {
+            return connectionInfo;
+        }
+
+        //Send the messages!
+        [FunctionName("messages")]
+        public static Task SendMessage(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "post")] ChatModel chat,
+            [SignalR(HubName = "chatHub")] IAsyncCollector<SignalRMessage> signalRMessages)
+        {
+            return signalRMessages.AddAsync(
+                new SignalRMessage
+                {
+                    Target = "clientMessage",
+                    Arguments = new[] { chat }
+                });
+        }
+    }
+}

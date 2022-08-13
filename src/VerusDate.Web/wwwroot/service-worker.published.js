a81263756d58d85b8013cb59a54dfee50f6ee755
Dhiogo Acioli <dhiogoacioli@outlook.com>
2022-08-13 12:07:12 -0300

    ..

--- src/VerusDate.Web/wwwroot/service-worker.published.js
+++ src/VerusDate.Web/wwwroot/service-worker.published.js
@@ -38,7 +38,7 @@ async function onFetch(event) {
     if (event.request.method === 'GET') {
         // For all navigation requests, try to serve index.html from cache
         // If you need some URLs to be server-rendered, edit the following check to exclude those URLs
-        const shouldServeIndexHtml = event.request.mode === 'navigate' && !event.request.url.includes('/.auth/login/');
+        const shouldServeIndexHtml = event.request.mode === 'navigate' && !event.request.url.includes('/.auth/');
 
         const request = shouldServeIndexHtml ? 'index.html' : event.request;
         const cache = await caches.open(cacheName);
